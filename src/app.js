b426a72d27191148710841256c081a873ec6bb37
kiaruh <nikoz.li@gmail.com>
2021-07-05 23:31:18 -0300

    agregue upload de user

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const path = require('path');
 const method = require('method-override');
 const session = require('express-session');
 const cookies = require('cookie-parser');
+const bodyParser = require('body-parser')
 
 const app = express();
 
@@ -12,27 +13,30 @@ const { send } = require('process');
 
 
 //session
-app.use(session({secret: "quboverde",resave: false,saveUninitialized: false}))
+app.use(session({secret: "quboverde",resave: false,saveUninitialized: false,}))
 
 //middlewares
 // app.use(userLoggedMiddleware)
-app.use(express.urlencoded({ extended: false }))
 app.use(cookies())
+app.use(express.urlencoded({ extended: false }))
+
+//Public Access
+app.use(express.static(path.resolve(__dirname,"../public"))); //igual que antes pero con un . adicional
+
+//Data configuration (Importante: debe estar antes de las rutas)
+app.use(express.urlencoded({extended:false})) //permite procesar info de un formulario (esta dos veces, despues sacamos uno)
+app.use(method("_method")) //parámetro debe coincidir con la ruta en el formulario ?_method=PUT/PATCH/DELETE
+
 
 //Server Start
 app.set('port',process.env.PORT || 3000) //establece el puerto que nos da el server o el 3000
 app.listen(app.get('port'),() => console.log('conectado a http://localhost:' + app.get('port')));//levanta la pagina
 
-//Public Access
-app.use(express.static(path.resolve(__dirname,"../public"))); //igual que antes pero con un . adicional
 
 //View Engine
 app.set("view engine","ejs");
 app.set("views",path.resolve(__dirname,"views"));
 
-//Data configuration (Importante: debe estar antes de las rutas)
-app.use(express.urlencoded({extended:false})) //permite procesar info de un formulario (esta dos veces, despues sacamos uno)
-app.use(method("_method")) //parámetro debe coincidir con la ruta en el formulario ?_method=PUT/PATCH/DELETE
 
 
 //Routes
@@ -47,11 +51,3 @@ app.use('/',rutasMain);
 app.use('/products',rutasProducts);
 app.use('/users',rutasUsers);
 app.use('/checkout',rutasCheckout);
-
-
-// app.get("/newprod", (req, res) => {
-//     res.sendFile(path.join(__dirname, "../view", "product_new.html")); // get cart
-// });
-
-//Para los formularios
-//npm i method-override
--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,7 +1,8 @@
 const bcryptjs = require("bcryptjs")
 const {validationResult} = require("express-validator")
+const { send } = require("process")
 
-const User = require("../models/productModel")
+const User = require("../models/userModel")
 
 
 const usercontroller = {
@@ -9,20 +10,25 @@ const usercontroller = {
     register: (req,res) => res.render("users/register"), // get register
 
     processRegister: (req,res) => {
-        const resultValidation = validationResult(req)
-        if(resultValidation.erros.length > 0){
-            return res.render("users/register", {erros: resultValidation.mapped(), oldData: req.body})
-        }
-        let userInDB = User.findByField('email', req.body.email)
+        // const resultValidation = validationResult(req)
+        // if(resultValidation.erros.length > 0){
+        //     return res.render("users/register", {errors: resultValidation.mapped(), oldData: req.body})
+        // }
+        // let userInDB = User.findByField('email', req.body.email)
 
-        if(userInDB) {
-            return res.render("users/register", {errors: { email: { msg: "Este mail ya fue utilizado" } }, oldData: req.body})
-        }
-        let userTocreate = { ...req.body, password: bcryptjs.hashSync(req.body.password, 10), avatar: req.file.filename }
+        // if(userInDB) {
+        //     return res.render("users/register", {errors: { email: { msg: "Este mail ya fue utilizado" } }, oldData: req.body})
+        // }
 
-        User.create(userTocreate)
+       
 
-        return res.redirect("users/login")
+    
+    let userTocreate = { ...req.body,password: bcryptjs.hashSync(req.body.password, 10)};
+
+        
+    let newUser =  User.create(userTocreate);
+    return res.redirect("/users/login")
+   
 
     },
 
