eaf17c77c9abc22d4b8311e278e206d102e33e02
kiaruh <nikoz.li@gmail.com>
2021-07-27 01:56:43 -0300

    middleware de validacion de registro funcionando mini cambio

--- src/routes/users.js
+++ src/routes/users.js
@@ -27,31 +27,6 @@ const mds = multer.diskStorage({
 });
 const fileUpload = multer({storage: mds});
 
-// const validations = [ 
-//     body('username').notEmpty().withMessage('Tienes que escribir un usuario'),
-//     body('birthdate').notEmpty().withMessage('Tienes que seleccionar una fecha'),
-//     body('firstname').notEmpty().withMessage('Tienes que escribir un nombre'),
-//     body('lastname').notEmpty().withMessage('Tienes que escribir un apellido'),
-//     body('email').notEmpty().withMessage('Tienes que escribir un email valido')
-//     .bail().isEmail().withMessage('Debes utilizar un formato del tipo email'),
-//     body('password').notEmpty().withMessage('Tienes que escribir una constraseña'),
-//     body('avatar').custom((value, { req }) => { 
-//         let file = req.file
-//         let acceptedExtensions = ['.jpg','.png','.gif']
-//         if(!file) {
-//             throw new Error('Tienes que subir una imagen')
-//         } else {
-//             let fileExtension = path.extname(file.originalname)
-//             if(!acceptedExtensions.includes(fileExtension)){
-//                 throw new Error(`Formato valido ${acceptedExtensions.join(', ')}`)
-//             }
-//         }
-        
-//         return true
-//     })
-
-// ]
-
 //Route
 router.get("/register", controller.register)
 router.post("/register",validations, controller.processRegister)
