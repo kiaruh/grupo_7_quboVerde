507201783abb49b40f3b988a4202d407ea9dbb31
kiaruh <nikoz.li@gmail.com>
2021-09-16 17:48:44 -0300

    validador de login

--- public/js/loginForm.js
+++ public/js/loginForm.js
@@ -1,7 +1,28 @@
-window.addEventListener('form', function(){
-    let form = document.querySelector('email')
+window.addEventListener('load', function(){
+    let form = document.querySelector('.form-login')
+    let mensaje = document.querySelector('.mensaje')
+    let email = form.email
+    let errors = []
 
-    form.addEventListener('focus', function() {
-        console.log(1)
+    email.addEventListener('input', validateEmail, false);
+    email.addEventListener('blur', validateEmail, false);
+
+    let regexEmail = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/
+
+    form.addEventListener('submit', (e) => {
+        e.preventDefault()
+        validateEmail() 
     })
+
+    function validateEmail(e) {
+        if (!email.value) {
+            mensaje.innerHTML = 'Escribe tu Email'
+        } else if (!regexEmail.test(email.value)) {
+            mensaje.innerHTML = 'El formano no es del tipo email'
+        } else {
+            mensaje.innerHTML = ' '
+            form.addEventListener('submit', () => form.submit())
+        }
+    }
+
 })
