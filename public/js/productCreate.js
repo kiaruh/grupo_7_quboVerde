7c8e9275ea744ad0907237d525ec91809098b5d0
kiaruh <nikoz.li@gmail.com>
2021-09-17 00:39:53 -0300

    create product val

---
+++ public/js/productCreate.js
@@ -0,0 +1,11 @@
+window.addEventListener('load',function(){
+    let form = document.querySelector('.register-registro')
+    
+
+
+    form.addEventListener('submit', (e)=>{
+        e.preventDefault()
+    })
+
+
+})
