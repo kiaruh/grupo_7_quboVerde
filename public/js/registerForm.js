6b18ab020073daf762f0921daf73f3201c701241
kiaruh <nikoz.li@gmail.com>
2021-09-17 00:15:40 -0300

    registerForm terminado

--- public/js/registerForm.js
+++ public/js/registerForm.js
@@ -1,41 +1,63 @@
 window.addEventListener('load', function (){
-    let form = document.querySelector('form')
-    let user = form.user
-    let date = form.date
-    let name = form.name
-    let lastname = form.lastname
-    let email = form.email
     
-    let danger = document.querySelector('.text-danger')
+    let form = document.querySelector('.form-register')
+
+    let user = document.querySelector("input[name='user']")
+    let name = document.querySelector("input[name='firstname']")
+    let lastname = document.querySelector("input[name='lastname']")
+    let email = document.querySelector("input[name='email']")
+    let password = document.querySelector("input[name='password']")
+    
     let msgUser = document.querySelector('.msg-user')
-    let msgDate = document.querySelector('.msg-user')
-    let msgName = document.querySelector('.msg-user')
-    let msgLastname = document.querySelector('.msg-user')
-    let msgEmail = document.querySelector('.msg-user')
+    let msgName = document.querySelector('.msg-name')
+    let msgLastname = document.querySelector('.msg-lastname')
+    let msgEmail = document.querySelector('.msg-email')
+    let msgPsw  = document.querySelector('.msg-psw')
+
 
     email.addEventListener('input', validateEmail, false)
     email.addEventListener('blur', validateEmail, false)
 
     let regexEmail = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/
 
+
+    
     form.addEventListener('submit', (e) => {
-        e.preventDefault()
-        validateEmail() 
+        e.preventDefault() 
+        if(isEmpty(user,msgUser)){
+            if(isEmpty(name,msgName)){
+                if(isEmpty(lastname,msgLastname)){
+                    if(validateEmail()){
+                        if(isEmpty(password,msgPsw)){
+                            form.submit()
+                        }
+                    }
+                }
+            }
+        }
     })
 
-    function validateEmail(e) {
+    function validateEmail() {
         if (!email.value) {
             msgEmail.innerHTML = 'Escribe tu Email'
+            return false
         } else if (!regexEmail.test(email.value)) {
-            danger.innerHTML = ' '
             msgEmail.innerHTML = 'El formano no es del tipo email'
+            return false
         } else {
-            msgEmail.innerHTML = ' '
-            form.addEventListener('submit', () => form.submit())
+            msgEmail.innerHTML = ''
+            return true
+
         }
     }
 
-
-
-
+    function isEmpty(input,output){
+        if(!input.value){
+            output.innerHTML = 'El campo esta vacio'
+            return false
+        }else {
+            output.innerHTML = ''
+            return true
+        }
+    }
 })
