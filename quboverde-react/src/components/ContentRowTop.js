563a6f54457c4a1ff8dff414c749a92c02666446
Marina Yu <arqmarinayu@gmail.com>
2021-09-22 23:06:13 -0300

    merge

--- quboverde-react/src/components/ContentRowTop.js
+++ quboverde-react/src/components/ContentRowTop.js
@@ -34,13 +34,14 @@ function ContentRowTop(){
 						<div className="col-lg-6 mb-4">
 							<div className="card shadow mb-4">
 								<div className="card-header py-3">
-									<h5 className="m-0 font-weight-bold text-gray-800">Último producto cargado</h5>
+									<h5 className="m-0 font-weight-bold text-gray-800">Producto estrella</h5>
 								</div>
 								<div className="card-body">
 									<div className="text-center">
-										<img className="img-fluid px-3 px-sm-4 mt-3 mb-4" style={{width: 40 +'rem'}} src={imagenFondo} alt=" Star Wars - Mandalorian "/>
+										<img className="img-fluid px-3 px-sm-4 mt-3 mb-4" style={{width: 40 +'rem'}} src={imagenFondo} alt=" planta "/>
 									</div>
-									<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, consequatur explicabo officia inventore libero veritatis iure voluptate reiciendis a magnam, vitae, aperiam voluptatum non corporis quae dolorem culpa citationem ratione aperiam voluptatum non corporis ratione aperiam voluptatum quae dolorem culpa ratione aperiam voluptatum?</p>
+									<h4>Cornstalk</h4>
+									<p>Tener esta exuberante belleza tropical en tu living es lo más parecido a estar de vacaciones. Tip: Mantenerla en clima cálido y húmedo que tanto le gusta.</p>
 									<a className="btn btn-danger" target="_blank" rel="nofollow" href="/">Ver detalle de producto</a>
 								</div>
 							</div>
--- src/database/config/config.js
+++ src/database/config/config.js
@@ -5,6 +5,7 @@ module.exports = {
     "database": "qv",
     "host": "127.0.0.1",
     "dialect": "mysql",
+    port: 8080
   },
   "test": {
     "username": "root",
